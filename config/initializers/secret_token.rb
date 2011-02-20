a9eb97eeef8090d63513cfd32a92dc4b7b66599a
Jair Rillo Junior <jrjuniorsp@yahoo.com.br>
2011-02-20 17:04:09 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JRCadastros::Application.config.secret_token = '7b03decde48e910ecdcdc9d3a67a3f25d5820512d0bd9cfb3f7b2e0bde612b78d57ee2e276fda983953db737ccd0e6efc11b39f3cbe30da66b4ea2d344591a20'
